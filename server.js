97374aaab454bd9008c4964df6df748fef49ed8a
Sean Stoddart <stodsy@hotmail.com>
2016-06-11 08:01:05 +1000

    Initialise repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log(req.method);
+		next();
+	}
+
+};
+
+
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res){
+	res.send('This is about us!');
+
+});
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function (){
+	console.log('Express server started on port: ' + port);
+
+});
