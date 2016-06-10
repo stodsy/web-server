1784d4490aa814a43c11e55d86730797a86983ed
Sean Stoddart <stodsy@hotmail.com>
2016-06-11 08:02:36 +1000

    Initialise repo

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about', middleware.requireAuthentication, function (req, res){
-	res.send('This is about us!');
+	res.send('This is about us!!!!');
 
 });
 app.use(express.static(__dirname + '/public'));
